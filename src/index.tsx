fced4ffb5fcf93c96aaad4b13e1a3ed143d92e9c
Peter van Dijk <petervandijk@gmail.com>
2018-11-12 13:39:59 +0100

    basic test

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,12 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 // import Hello from './Hello'
-import StatefulHello from './components/StatefulHello'
+import Hello from './components/Hello'
 import './css/style.css';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
-  <StatefulHello name="World" enthusiasmLevel={3} />,
+  <Hello name="World" enthusiasmLevel={0} />,
   document.getElementById('root') as HTMLElement
 );
 registerServiceWorker();
